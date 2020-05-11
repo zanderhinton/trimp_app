0b1b7accc1b257a08de3f56d54d00545c9304d1f
Alexander Hinton <ahinton1991@gmail.com>
2020-05-10 20:11:22 -0700

    edits

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.title = "Player Game-Load Monitoring"
 # read in summarized data
 summary_home_1 = pd.read_csv("data/Summary_Stats/home_game_1.csv")
 summary_home_2 = pd.read_csv("data/Summary_Stats/home_game_2.csv")
-summary_away_2 = pd.read_csv("data/Summary_Stats/away_game_1.csv")
+summary_away_1 = pd.read_csv("data/Summary_Stats/away_game_1.csv")
 summary_away_2 = pd.read_csv("data/Summary_Stats/away_game_2.csv")
 
 def make_minutes_plot(summary_df):
@@ -41,7 +41,7 @@ def make_distance_plot(summary_df):
 def make_trimp_plot(summary_df):
     chart = alt.Chart(summary_df.reset_index()).mark_bar(color = 'red').encode(
         x = alt.X('index:O', title = "player"),
-        y = alt.Y('trimp_score:Q', title = "TRIMP score")
+        y = alt.Y('trimp_score:Q', title = "TRIMP score (AU)")
         ).properties(title = "TRIMP score by player")
     return chart
 
@@ -91,9 +91,11 @@ CONTENT = dbc.Container(
         html.P("The TRIMP (TRaining IMpulse) [reference #3]  score framework has been used to assign a player load \
             score to each player, for each game. \
             The TRIMP model has been used in endurance sports to quantify the load of a given training \
-            session on individual athletes. In it's most basic form, the TRIMP score of a session will be the RPE \
+            session on individual athletes. The duration or volume of a training session on it's own does not \
+            fully capture the difficulty of the session, as it fails to account for session intensity. \
+            In it's most basic form, the TRIMP score of a session is the Rate of Perceived Exertion (RPE) \
             of the session multiplied by the duration, and it is measured in Arbitrary Units (AU).  \
-            As a proxy for RPE, I have used arbitrary speed cut-offs."),
+            As a proxy for RPE, I have used arbitrary speed cut-offs to determine RPE zones."),
         dbc.Row(dbc.Col(html.Ul(id='my-list', 
             children=["RPE 1: speed less than 1 m/s",
             html.Br(),
@@ -114,7 +116,7 @@ CONTENT = dbc.Container(
             "RPE 9: speed between 8-9 m/s",
             html.Br(),
             "RPE 10: speed > 9 m/s"]))),
-        html.P("The number of seconds in each of these RPE bins is then multiplied by the RPE level to get a total TRIMP Score."),
+        html.P("The number of seconds in each of the RPE bins is then multiplied by the RPE level (1-10) to get a TRIMP Score."),
         dbc.Row(dbc.Col(html.H4('Data'))),
         html.P("Currently there are only two games in the demo. The games used in the analysis come from the Sample data on the Metrica Sports \
             Github page [reference #1]. The games and players are anonymized in the data."),
@@ -173,8 +175,8 @@ CONTENT = dbc.Container(
         dbc.Row(dbc.Col(html.H4('Discussion'))),
         html.P("In this demo the TRIMP score tracks quite closely with the distance ran. \
             However there are a few exceptions where it provides additional and useful information. For example, \
-            in Game 1 for the Home team player 11 covers about 0.5 the distance of player 10 \
-            but has a TRIMP score about 0.75 as large as player 10's, indicating player 11 must have had some high intensity efforts. \
+            in Game 1 for the Home team, player 2 covers about 0.5 the distance of player 1 \
+            but has a TRIMP score about 0.75 as large as player 1's, indicating player 2 must have had some high intensity efforts. \
             The TRIMP framework here could also be improved by tailoring the RPE cut-off speeds to each individual, as well as \
             potentially using other metrics such as acceleration."),
         html.Br(),
